358bfa217656ffcdcedfef02c92e452ae42bbf12
Romain Tello <romain.tello@gmail.com>
2020-03-27 10:41:13 +0100

    Beautify series page

--- index.php
+++ index.php
@@ -17,18 +17,30 @@ $tvShow = getTvShow(MY_TV_SHOWS[1]);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My beautiful app</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/b07d4a60b8.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1><?= $tvShow->name ?></h1>
-    <a href="<?= $tvShow->homepage ?>">Homepage</a>
-    <div>
-        <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2<?= $tvShow->poster_path ?>" />
+    <div class="container">
+        <div class="jumbotron">
+            <h1>
+                <?= $tvShow->name ?>
+                <a href="<?= $tvShow->homepage ?>">
+                    <i class="fas fa-external-link-alt"></i>
+                </a>
+            </h1>
+            <div>
+                <img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2<?= $tvShow->poster_path ?>" />
+            </div>
+        </div>
+        <h2>Seasons</h2>
+        <ul class="list-group">
+            <?php foreach($tvShow->seasons as $season): ?>
+            <li class="list-group-item">
+                Season <?= $season->season_number ?>: <?= $season->name ?>
+            </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
-    <h2>Seasons</h2>
-    <ul>
-        <?php foreach($tvShow->seasons as $season): ?>
-        <li>Season <?= $season->season_number ?>: <?= $season->name ?></li>
-        <?php endforeach; ?>
-    </ul>
 </body>
 </html>
